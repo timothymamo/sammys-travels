9a045b0c230722f2351f125eaacf0a1a4ff1daf8
Timothy Mamo <tim@themamo.com>
2023-09-29 10:41:07 +0200

    Added time to site check output

--- check-site.sh
+++ check-site.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 while true; do
+  var=$(date +"%H:%M:%S")
   if [ "$(curl --silent https://sammys-travels.devreltim.io/api/health)" == '{"status":200}' ]; then
-    echo "Site is UP"
+    echo "Site is UP - $var"
   else
-    echo "Site is DOWN"
+    echo "Site is DOWN - $var"
   fi
   sleep 5
 done
